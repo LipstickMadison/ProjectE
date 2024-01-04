75edce81903d9b39971074879ffeb46cf6cd8ada
vanessawilliams <vanessawilliams@localhost>
2024-01-03 21:04:25 -0500

    initial commit

---
+++ src/e/DemoE.java
@@ -0,0 +1,14 @@
+package e;
+
+public class DemoE {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Friday");
+		
+		System.out.println("day 1");
+		System.out.println("day 2");
+		System.out.println("day 3");
+	}
+
+}
